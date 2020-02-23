582ba7eae422ad78fbfa94bee66e8e9f6752bc50
Arif Ullah <aullah1@hotmail.co.uk>
2020-02-23 16:51:53 +0000

    Create magma.php

---
+++ magma.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Magma Framework
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Magma Framework
+Description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+Author: Exertion
+Version: 1.0.0
+Author URI: https://exertion.co
+*/
+require_once __DIR__ . '/vendor/autoload.php';
