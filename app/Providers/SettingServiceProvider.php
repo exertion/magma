c0dd172e19d68e42ee9b215028db14d3c77405ab
Arif Ullah <aullah1@hotmail.co.uk>
2020-04-01 18:14:20 +0100

    Implement SettingServiceProvider, which provides an elegant way to create a fully functional settings page.

---
+++ app/Providers/SettingServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use Spotlight\Administration\Settings;
+
+class SettingServiceProvider
+{
+    /**
+     * Indicates if the plugin will have a settings page.
+     *
+     * @var bool
+     */
+    protected $enabled = true;
+
+    public function __construct()
+    {
+        new Settings($this->enabled);
+    }
+
+    /**
+     * Finish configuring Spark for the application.
+     *
+     * @return void
+     */
+     public function booted()
+     {
+        Settings::tab('General Settings')
+                ->option([ 'name' => 'First Field', 'type' => 'text', 'description' => 'Hey, this is an example text field!' ])
+                ->option([ 'name' => 'Second Field', 'default' => 'A default value!' ])
+                ->option([ 'name' => 'Checkbox', 'type' => 'checkbox', 'description' => 'Tick this box for no reason!' ]);
+
+        Settings::tab('Additional Settings', 'additional')
+            ->option([ 'name' => 'Drop-Down', 'type' => 'select', 'options' => [ 'first' => 'First Option', 'second' => 'Second Option' ] ]);
+
+        Settings::init();
+     }
+}
