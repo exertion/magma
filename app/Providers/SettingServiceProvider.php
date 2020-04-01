2bd4cd08b878476028abf958ba688dcdafb24ad0
Arif Ullah <aullah1@hotmail.co.uk>
2020-04-01 20:22:19 +0100

    Fix indentation on SettingServiceProvider

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -19,20 +19,20 @@ class SettingServiceProvider
     }
 
     /**
-     * Configure the settings page declaring tabs and options.
-     *
-     * @return void
-     */
-     public function booted()
-     {
+    * Configure the settings page declaring tabs and options.
+    *
+    * @return void
+    */
+    public function booted()
+    {
         Settings::tab('General Settings')
                 ->option([ 'name' => 'First Field', 'type' => 'text', 'description' => 'Hey, this is an example text field!' ])
                 ->option([ 'name' => 'Second Field', 'default' => 'A default value!' ])
                 ->option([ 'name' => 'Checkbox', 'type' => 'checkbox', 'description' => 'Tick this box for no reason!' ]);
 
         Settings::tab('Additional Settings', 'additional')
-            ->option([ 'name' => 'Drop-Down', 'type' => 'select', 'options' => [ 'first' => 'First Option', 'second' => 'Second Option' ] ]);
+                ->option([ 'name' => 'Drop-Down', 'type' => 'select', 'options' => [ 'first' => 'First Option', 'second' => 'Second Option' ] ]);
 
         Settings::init();
-     }
+    }
 }
