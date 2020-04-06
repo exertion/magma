a7b2e4acf9ae0f2d5e2f4ea23112ce96515ef8ac
Arif Ullah <aullah1@hotmail.co.uk>
2020-04-07 00:46:27 +0100

    Turn on the Spotlight!
    Initiate the Spotlight Foundation, enabling this plugin framework to use the magma-framework. Also define a global config() function, similar to Laravel's.

--- magma.php
+++ magma.php
@@ -11,3 +11,10 @@ Version: 1.0.0
 Author URI: https://exertion.co
 */
 require_once __DIR__ . '/vendor/autoload.php';
+
+$plugin = new Spotlight\Foundation\Application(__DIR__);
+
+function config($option, $default = null) {
+    $config = new Spotlight\Foundation\Config(__DIR__, __FILE__);
+    return $config->config($option, $default = null);
+}
